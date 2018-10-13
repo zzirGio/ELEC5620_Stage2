8e62dd2a8b81840836515824124dc7def9d8ba37
Sergio Mesina <smes3708@uni.sydney.edu.au>
2018-10-13 13:35:20 +1100

    feature: implement company products
    - Implemented Products List
    - Implemented View Product Information
    - Implemented Modify Product Information

--- company/views.py
+++ company/views.py
@@ -3,8 +3,9 @@ from django.views import generic
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from .decorators import company_user_required
-from .forms import CompanySignUpForm, AddProductForm
+from .forms import CompanySignUpForm, AddProductForm, UpdateProductForm
 from daintree.models import User, Product, Company
 
 
@@ -36,14 +37,14 @@ class AnalyseCompanyView(generic.TemplateView):
 
 @method_decorator([login_required, company_user_required], name='dispatch')
 class AddProductSuccessView(generic.TemplateView):
-    template_name = 'company/addproduct_success.html'
+    template_name = 'company/product_saved_success.html'
 
 
 @method_decorator([login_required, company_user_required], name='dispatch')
 class AddProductView(generic.CreateView):
     model = Product
     form_class = AddProductForm
-    success_url = reverse_lazy('company_add_product_success')
+    success_url = reverse_lazy('company_product_save_success')
     template_name = 'company/addproduct.html'
 
     def form_valid(self, form):
@@ -51,3 +52,35 @@ class AddProductView(generic.CreateView):
         newproduct.company = Company.objects.get(user=self.request.user)
         newproduct.save()
         return HttpResponseRedirect(self.success_url)
+
+
+@method_decorator([login_required, company_user_required], name='dispatch')
+class ProductsListView(generic.TemplateView):
+    template_name = 'company/productlist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        company = Company.objects.get(user=self.request.user)
+        context['products'] = company.get_products()
+        return context
+
+
+# @method_decorator([login_required, company_user_required], name='dispatch')
+@login_required
+@company_user_required
+def product_view(request, id=None):
+    company = get_object_or_404(Company, user=request.user)
+    product = get_object_or_404(Product, id=id)
+
+    if request.method == 'POST':
+        form = UpdateProductForm(request.POST, instance=product)
+        if form.is_valid():
+            updated_product = form.save()
+            updated_product.company = company
+            updated_product.save()
+            return HttpResponseRedirect(reverse_lazy('company_product_save_success'))
+
+    else:
+        form = UpdateProductForm(instance=product)
+
+    return render(request, 'company/product.html', context={'form': form, 'product': product})
