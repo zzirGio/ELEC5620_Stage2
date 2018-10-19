0b96a310d5fefb0b687bc86d925357fb4492eb91
Sergio Mesina <smes3708@uni.sydney.edu.au>
2018-10-19 21:45:37 +1100

    task: style company product information page
    - Styled Company Product Information Page in Company Dashboard

--- company/forms.py
+++ company/forms.py
@@ -96,12 +96,52 @@ class AddProductForm(forms.ModelForm):
 
 
 class UpdateProductForm(forms.ModelForm):
-    name = forms.CharField(max_length=255)
-    stock = forms.IntegerField(min_value=0)
-    price = forms.FloatField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label=None)
-    description = forms.CharField(max_length=1024, widget=forms.Textarea, required=False)
-    # company = forms.HiddenInput()
+    name = forms.CharField(
+        label='Name',
+        max_length=255,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'class': 'form-input name', 'name': 'name'}
+        )
+    )
+    stock = forms.IntegerField(
+        label='Stock',
+        min_value=0,
+        required=True,
+        widget=forms.NumberInput(
+            attrs={'class': 'form-input stock', 'name': 'stock'}
+        )
+    )
+    price = forms.FloatField(
+        label='Price ($)',
+        required=True,
+        widget=forms.NumberInput(
+            attrs={'class': 'form-input price', 'name': 'price'}
+        )
+    )
+    category = forms.ModelChoiceField(
+        label='Category',
+        required=True,
+        queryset=Category.objects.all(),
+        empty_label=None,
+        widget=forms.Select(
+            attrs={'class': 'form-input category', 'name': 'category'}
+        )
+    )
+
+    description = forms.CharField(
+        label='Description',
+        max_length=1024,
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-input description',
+                'name': 'description',
+                'rows': 4,
+                'cols': 40
+            }
+        )
+    )
 
     class Meta:
         model = Product
